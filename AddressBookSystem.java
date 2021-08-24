e7f1d614aa965e164c1e7dde848c07b006db6474
asimbinahammed <asim242370@gmail.com>
2021-08-24 20:35:04 +0530

    Adding Details into array

--- AddressBookSystem.java
+++ AddressBookSystem.java
@@ -8,32 +8,54 @@ public class AddressBookSystem {
 	 * scanning details from user
 	 */
 	public static void main(String[] args) {
+		int i=0;
+		String array[]=new String[8];
+		
 		Scanner sc = new Scanner(System.in);
-	    System.out.print("Enter firstname");
+	    System.out.print("Enter firstname: ");
 	    String fname = sc.nextLine();
+	    array[i]=fname;
+	    i++;
 	    
-	    System.out.print("Enter Last name");
+	    System.out.print("Enter Last name: ");
 	    String lname = sc.nextLine();
+	    array[i]=lname;
+	    i++;
 	    
-	    System.out.print("Enter address");
+	    System.out.print("Enter address: ");
 	    String address = sc.nextLine();
+	    array[i]=address;
+	    i++;
 	    
-	    System.out.print("Enter City");
+	    System.out.print("Enter City: ");
 	    String city = sc.nextLine();
+	    array[i]=city;
+	    i++;
 	    
-	    System.out.print("Enter state");
+	    System.out.print("Enter state: ");
 	    String state = sc.nextLine();
+	    array[i]=state;
+	    i++;
 	    
-	    System.out.print("Enter zip");
-	    int zip = sc.nextInt();
+	    System.out.print("Enter zip: ");
+	    String zip = sc.nextLine();
+	    array[i]=zip;
+	    i++;
 	    
-	    System.out.print("Enter phone number");
-	    Long Phno = sc.nextLong();
+	    System.out.print("Enter phone number: ");
+	    String phno = sc.nextLine();
+	    array[i]=phno;
+	    i++;
 	    
-	    System.out.print("Enter Email");
+	    System.out.print("Enter Email: ");
 	    String email = sc.nextLine();
+	    array[i]=email;
+	    i++;
 	    
 	    
+	    for (int j = 0; j < array.length; j++) {
+	    	System.out.println( array[j]+ " ");  
+	    }
 	}
-
+	    
 }
