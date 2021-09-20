1d6aca85367e3282c8d12202356657f80f5dd399
asimbinahammed <asim242370@gmail.com>
2021-09-20 12:12:14 +0530

    Refactorimg:Add multiple address book

--- AddressBook.java
+++ AddressBook.java
@@ -6,143 +6,143 @@ import java.util.Scanner;
 
 public class AddressBook {
 
-	Scanner scanner = new Scanner(System.in);
-	ArrayList<PersonInfo> contact;
+    Scanner scanner = new Scanner(System.in);
+    ArrayList<PersonInfo> contact;
 
-	// declaring array list
-	public AddressBook() {
-		contact = new ArrayList<PersonInfo>();
-	}
+    // declaring array list
+    public AddressBook() {
+        contact = new ArrayList<PersonInfo>();
+    }
 
-	// add new person
-	public void addPerson() {
-		System.out.print("Enter first name: ");
-		String firstName = scanner.next();
-		System.out.println("Enter the Last name: ");
-		String lastName = scanner.next();
-		System.out.print("Enter complete address: ");
-		String address = scanner.next();
-		System.out.print("Enter city: ");
-		String city = scanner.next();
-		System.out.println("Enter state: ");
-		String state = scanner.next();
-		System.out.println("Enter ZIP: ");
-		String zip = scanner.next();
-		System.out.println("Enter phone number: ");
-		String phoneNumber = scanner.next();
-		System.out.println("Enter email id: ");
-		String email = scanner.next();
+    public static void main(String[] args) {
 
-		//construct new person object
-				PersonInfo person = new PersonInfo(firstName, lastName, address, city, state, zip, phoneNumber, email);
-				System.out.println(person);
+        //declared new hash map for multiple address book storing
+        HashMap<String, AddressBook> addressBook = new HashMap<String, AddressBook>();
 
-				//add PersonInfo object to arraylist
-				contact.add(person);
-				System.out.println("Contact added successfully\n");
-	}
+        System.out.println("Welcome to Address Book");
+        @SuppressWarnings("resource")
+        Scanner input = new Scanner(System.in);
+        String firstName, lastName;
 
-	// edit person details
-	public void editPerson(String firstName, String lastName) {
-		for (int i = 0; i <= contact.size(); i++) {
-			PersonInfo person = (PersonInfo) contact.get(i);
-			if (firstName.equals(person.getFirstName()) && lastName.equals(person.getLastName())) {
-				System.out.print("Enter address: ");
-				String address = scanner.next();
-				System.out.print("Enter city: ");
-				String city = scanner.next();
-				System.out.println("Enter state: ");
-				String state = scanner.next();
-				System.out.println("Enter ZIP: ");
-				String zip = scanner.next();
-				System.out.println("Enter phone number: ");
-				String phoneNumber = scanner.next();
-				System.out.println("Enter email id: ");
-				String email = scanner.next();
+        AddressBook address = new AddressBook();
 
-				person = new PersonInfo(person.getFirstName(), person.getLastName(), address, city, state, zip,
-						phoneNumber, email);
-				System.out.println(person);
-				System.out.println("Contact updated successfully\n");
-				contact.add(person);
-				contact.remove(i);
-				break;
-			} else {
-				System.out.println(person);
-				System.out.println("firstname and lastname didnt match");
-			}
+        while (true) {
+            System.out.println("Enter name of Address Book");
+            String addressBookName = input.next();
 
-		}
-	}
+            addressBook.put(addressBookName, address);
 
-	// delete person
-	public void deletePerson(String firstName, String lastName) {
-		for (int i = 0; i <= contact.size(); i++) {
-			PersonInfo person = (PersonInfo)contact.get(i);
-			if (firstName.equals(person.getFirstName()) && lastName.equals(person.getLastName())) {
-				person = null;
-				System.out.println(person);
-				System.out.println("Contact Deleted successfully\n");
-				contact.remove(i);
-			} else {
-				System.out.println(person);
-				System.out.println("firstname and lastname didnt match");
-			}
+            System.out.println("Select an action..");
+            System.out.println("1. Add a person");
+            System.out.println("2. Edit information");
+            System.out.println("3. Delete a person");
+            System.out.println("4. Quit");
 
-		}
-	}
+            int choice = input.nextInt();
+            switch (choice) {
+                case 1:
+                    addressBook.get(addressBookName).addPerson();
+                    System.out.println("\n");
+                    break;
+                case 2:
+                    System.out.print("Enter first name and last name of the person to edit the contact: ");
+                    firstName = input.next();
+                    lastName = input.next();
+                    addressBook.get(addressBookName).editPerson(firstName, lastName);
+                    System.out.println("\n");
+                    break;
+                case 3:
+                    System.out.print("Enter first and last name of the person to delete the contact: ");
+                    firstName = input.next();
+                    lastName = input.next();
+                    addressBook.get(addressBookName).deletePerson(firstName, lastName);
+                    System.out.println("\n");
+                    break;
+                case 4:
+                    System.exit(0);
+                default:
+                    System.out.println("Enter a valid option");
+                    break;
+            }
+        }
 
-	public static void main(String[] args) {
-		
-		 //declared new hash map for multiple address book storing
-		HashMap<String,AddressBook> addressBook=new HashMap<String,AddressBook>();
-		
-		System.out.println("Welcome to Address Book");
-		@SuppressWarnings("resource")
-		Scanner input = new Scanner(System.in);
-		String firstName, lastName;
+    }
 
-		AddressBook address = new AddressBook();
+    // add new person
+    public void addPerson() {
+        System.out.print("Enter first name: ");
+        String firstName = scanner.next();
+        System.out.println("Enter the Last name: ");
+        String lastName = scanner.next();
+        System.out.print("Enter complete address: ");
+        String address = scanner.next();
+        System.out.print("Enter city: ");
+        String city = scanner.next();
+        System.out.println("Enter state: ");
+        String state = scanner.next();
+        System.out.println("Enter ZIP: ");
+        String zip = scanner.next();
+        System.out.println("Enter phone number: ");
+        String phoneNumber = scanner.next();
+        System.out.println("Enter email id: ");
+        String email = scanner.next();
 
-		while (true) {
-			System.out.println("Enter name of Address Book");
-	        String addressBookName = input.next();
-	        
-	        addressBook.put(addressBookName, address);
+        //construct new person object
+        PersonInfo person = new PersonInfo(firstName, lastName, address, city, state, zip, phoneNumber, email);
+        System.out.println(person);
 
-			System.out.println("Select an action..");
-			System.out.println("1. Add a person");
-			System.out.println("2. Edit information");
-			System.out.println("3. Delete a person");
-			System.out.println("4. Quit");
+        //add PersonInfo object to arraylist
+        contact.add(person);
+        System.out.println("Contact added successfully\n");
+    }
 
-			int choice = input.nextInt();
-			switch (choice) {
-			case 1:
-				addressBook.get(addressBookName).addPerson();
-				System.out.println("\n");
-				break;
-			case 2:
-				System.out.print("Enter first name and last name of the person to edit the contact: ");
-				firstName = input.next();
-				lastName = input.next();
-				addressBook.get(addressBookName).editPerson(firstName, lastName);
-				System.out.println("\n");
-				break;
-			case 3:
-				System.out.print("Enter first and last name of the person to delete the contact: ");
-				firstName = input.next();
-				lastName = input.next();
-				addressBook.get(addressBookName).deletePerson(firstName, lastName);
-				System.out.println("\n");
-				break;
-			case 4:
-				System.exit(0);
-			default:
-				System.out.println("Enter a valid option");
-				break;
-			}
-		}
+    // edit person details
+    public void editPerson(String firstName, String lastName) {
+        for (int i = 0; i <= contact.size(); i++) {
+            PersonInfo person = contact.get(i);
+            if (firstName.equals(person.getFirstName()) && lastName.equals(person.getLastName())) {
+                System.out.print("Enter address: ");
+                String address = scanner.next();
+                System.out.print("Enter city: ");
+                String city = scanner.next();
+                System.out.println("Enter state: ");
+                String state = scanner.next();
+                System.out.println("Enter ZIP: ");
+                String zip = scanner.next();
+                System.out.println("Enter phone number: ");
+                String phoneNumber = scanner.next();
+                System.out.println("Enter email id: ");
+                String email = scanner.next();
 
-	}
+                person = new PersonInfo(person.getFirstName(), person.getLastName(), address, city, state, zip,
+                        phoneNumber, email);
+                System.out.println(person);
+                System.out.println("Contact updated successfully\n");
+                contact.add(person);
+                contact.remove(i);
+                break;
+            } else {
+                System.out.println(person);
+                System.out.println("firstname and lastname didnt match");
+            }
+
+        }
+    }
+
+    // delete person
+    public void deletePerson(String firstName, String lastName) {
+        for (int i = 0; i <= contact.size(); i++) {
+            PersonInfo person = contact.get(i);
+            if (firstName.equals(person.getFirstName()) && lastName.equals(person.getLastName())) {
+                person = null;
+                System.out.println(person);
+                System.out.println("Contact Deleted successfully\n");
+                contact.remove(i);
+            } else {
+                System.out.println(person);
+                System.out.println("firstname and lastname didnt match");
+            }
+
+        }
+    }
 }
