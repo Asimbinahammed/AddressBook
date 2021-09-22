cb3480ebeef53a3b88ca3521433aa7914f0a5f5c
asimbinahammed <asim242370@gmail.com>
2021-09-22 22:34:21 +0530

    Sorting using city,state or zip

--- AddressBook.java
+++ AddressBook.java
@@ -160,14 +160,67 @@ public class AddressBook {
 	}
 
 	/*
-	UC10:Sort list by name
+	UC11:Sort list by name
 	input->list
 	output->sorted list
 	 */
 	public static void sortByName(){
 		AddressBook.sortBy(PersonInfo::getFirstName).forEach(System.out::println);
 	}
-	
+
+	/*
+	UC12:Sort list by city,state or zip
+	selecting sorting by which factor
+	 */
+	public static void sortByCityStateZip(){
+		System.out.println("    1.Sort by City ");
+		System.out.println("	2.Sort by State");
+		System.out.println("	3.Sort by Zip");
+		String menuOption;
+		menuOption = sc.nextLine();
+		switch (menuOption) {
+			case "1":
+				sortByCity();
+				break;
+			case "2":
+				sortByState();
+				break;
+			case "3":
+				sortByZip();
+				break;
+			default:
+				System.out.println("Invalid Input");
+		}
+	}
+
+	/*
+	UC12:Sort list by city
+	input->list
+	output->sorted list
+	 */
+	public static void sortByCity(){
+		AddressBook.sortBy(PersonInfo::getCity).forEach(System.out::println);
+	}
+
+	/*
+	UC12:Sort list by state
+	input->list
+	output->sorted list
+	 */
+	public static void sortByState(){
+		AddressBook.sortBy(PersonInfo::getState).forEach(System.out::println);
+	}
+
+	/*
+	UC12:Sort list by zip
+	input->list
+	output->sorted list
+	 */
+	public static void sortByZip(){
+		AddressBook.sortBy(PersonInfo::getZip).forEach(System.out::println);
+	}
+
+	//defining sorting from person info by comparing
 	public static List<PersonInfo> sortBy(Function<? super PersonInfo, ? extends String> key) {
 		return personInfo.stream().sorted(Comparator.comparing(key)).collect(Collectors.toList());
 	}
@@ -183,6 +236,7 @@ public class AddressBook {
 			System.out.println("	6.Search Person Using City");
 			System.out.println("	7.Count person in a city");
 			System.out.println("	8.Sort by Person's name");
+			System.out.println("	9.Sort by city ,state or zip");
 
 			menuOption = sc.nextLine();
 			switch (menuOption) {
@@ -215,6 +269,9 @@ public class AddressBook {
 					break;
 				case "8":
 					sortByName();
+					break;
+				case "9":
+					sortByCityStateZip();
 					break;
 				default:
 					System.out.println("Invalid Input");
